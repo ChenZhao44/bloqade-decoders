84ab4846f159c0f83fe076b0fc497ebdae94a8fe
Chen Zhao <czhao.cz44@gmail.com>
2026-09-02 23:00:47 +0800

    Add SinterMILPDecoder adapter (TDD green)

--- src/bloqade/decoders/sinter_interface.py
+++ src/bloqade/decoders/sinter_interface.py
@@ -1,2 +1,3 @@
 from ._decoders.mld.sinter_interface import SinterTableDecoder as SinterTableDecoder
 from ._decoders.mle.sinter_interface import SinterGurobiDecoder as SinterGurobiDecoder
+from ._decoders.milp.sinter_interface import SinterMILPDecoder as SinterMILPDecoder
